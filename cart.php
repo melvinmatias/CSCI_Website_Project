099aa12fe25d91acfe2a664fd882ff7e280e304c
Melvin Matias <44871742+melvinmatias@users.noreply.github.com>
2022-11-27 12:26:22 -0500

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,188 @@
+<!--Init setup------------------------------------------------------------------->
+<?php
+ session_start();
+ $connect = mysqli_connect("localhost", "root", "root", "csci250-project");
+ if(isset($_POST["add_to_cart"]))
+ {
+      if(isset($_SESSION["shopping_cart"]))
+      {
+           $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+           if(!in_array($_GET["id"], $item_array_id))
+           {
+                $count = count($_SESSION["shopping_cart"]);
+                $item_array = array(
+                     'item_id'               =>     $_GET["id"],
+                     'item_name'               =>     $_POST["hidden_name"],
+                     'item_price'          =>     $_POST["hidden_price"],
+                     'item_quantity'          =>     $_POST["quantity"]
+                );
+                $_SESSION["shopping_cart"][$count] = $item_array;
+           }
+           else
+           {
+                echo '<script>alert("Item Already Added, Remove Item to Reorder")</script>';
+                echo '<script>window.location="cart.php"</script>';
+           }
+      }
+      else
+      {
+           $item_array = array(
+                'item_id'               =>     $_GET["id"],
+                'item_name'               =>     $_POST["hidden_name"],
+                'item_price'          =>     $_POST["hidden_price"],
+                'item_quantity'          =>     $_POST["quantity"]
+           );
+           $_SESSION["shopping_cart"][0] = $item_array;
+      }
+ }
+ if(isset($_GET["action"]))
+ {
+      if($_GET["action"] == "delete")
+      {
+           foreach($_SESSION["shopping_cart"] as $keys => $values)
+           {
+                if($values["item_id"] == $_GET["id"])
+                {
+                     unset($_SESSION["shopping_cart"][$keys]);
+                     echo '<script>alert("Item Removed")</script>';
+                     echo '<script>window.location="cart.php"</script>';
+                }
+           }
+      }
+ }
+ ?>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <title>BasePro Cart</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body>
+    <!--Navigation Bar Code--------------------------------------------------------------------->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="projectIndex.html">Base<img src = 'logo.png' width="25" height="25">Pro</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!--Nav Items-->
+              <li class="nav-item">
+              <a class="nav-link" href="projectIndex.html">Home</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="shop.php">Shop</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Account</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="cart.php">Shopping Cart</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!--Title------------------>
+        <div align = center><h1 class="display-1" style= "font-family:'News GothicMT'">Base<img src = 'logo.png' width="100" height="100">Pro</h1></div>
+    
+    <!--Shopping Page---------------------------------------------------------------->
+    <!--Db connection-->
+    <div class="container">
+    <div class="grid text-center" style="--bs-columns: 3;">
+    
+    <!--Displaying Items-->
+    
+                
+    
+    </div>
+    <?php 
+    
+    ?>
+    <div style="clear:both"></div>
+                <br />
+                <h3>Order Details</h3>
+                <div class="table-responsive">
+                     <table class="table table-bordered">
+                          <tr>
+                               <th width="40%">Item Name</th>
+                               <th width="10%">Quantity</th>
+                               <th width="20%">Price</th>
+                               <th width="15%">Total</th>
+                               <th width="5%">Action</th>
+                          </tr>
+                          <?php
+                          if(!empty($_SESSION["shopping_cart"]))
+                          {
+                               $total = 0;
+                               foreach($_SESSION["shopping_cart"] as $keys => $values)
+                               {
+                          ?>
+                          <tr>
+                               <td><?php echo $values["item_name"]; ?></td>
+                               <td><?php echo $values["item_quantity"]; ?></td>
+                               <td>$ <?php echo $values["item_price"]; ?></td>
+                               <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
+                               <td><a href="cart.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+                          </tr>
+                          <?php
+                                    $total = $total + ($values["item_quantity"] * $values["item_price"]);
+                               }
+                          ?>
+                          <tr>
+                               <td colspan="3" align="right">Total</td>
+                               <td align="right">$ <?php echo number_format($total, 2); ?></td>
+                               <td></td>
+                          </tr>
+                          <?php
+                          }
+                          ?>
+                     </table>
+                </div>
+    
+
+    
+
+
+                        </div>
+    <!--Footer---------------------------------------------------------------------------------------------->
+    <footer class="text-center text-white bg-primary">
+      <div class="container p-4">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5>Base Pro Cares</h5>
+    
+            <p>
+              Here at Base Pro, we are passionate about baseball and <br> hope to share our passion with the world. We are flexible
+              <br>with returns to ensure our customers get the best service!
+            </p>
+          </div>
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5>Contact Us!</h5>
+              862-435-1344<br>
+              901-274-1109<br>
+              BasePros@baseball.com<br>
+              customersupport@baseball.com
+          </div>
+        </div>
+      </div>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        @ 2022 Copyright: Melvin Matias & Justin Pernell
+      </div>
+    </footer>
+   
+  </body>
+</html>
---
+++ shop.php
@@ -0,0 +1,214 @@
+<!--Init setup------------------------------------------------------------------->
+<?php
+ session_start();
+ $connect = mysqli_connect("localhost", "root", "root", "csci250-project");
+ if(isset($_POST["add_to_cart"]))
+ {
+      if(isset($_SESSION["shopping_cart"]))
+      {
+           $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+           if(!in_array($_GET["id"], $item_array_id))
+           {
+                $count = count($_SESSION["shopping_cart"]);
+                $item_array = array(
+                     'item_id'               =>     $_GET["id"],
+                     'item_name'               =>     $_POST["hidden_name"],
+                     'item_price'          =>     $_POST["hidden_price"],
+                     'item_quantity'          =>     $_POST["quantity"]
+                );
+                $_SESSION["shopping_cart"][$count] = $item_array;
+           }
+           else
+           {
+                echo '<script>alert("Item Already Added, Remove Item to Reorder")</script>';
+                echo '<script>window.location="shop.php"</script>';
+           }
+      }
+      else
+      {
+           $item_array = array(
+                'item_id'               =>     $_GET["id"],
+                'item_name'               =>     $_POST["hidden_name"],
+                'item_price'          =>     $_POST["hidden_price"],
+                'item_quantity'          =>     $_POST["quantity"]
+           );
+           $_SESSION["shopping_cart"][0] = $item_array;
+      }
+ }
+ if(isset($_GET["action"]))
+ {
+      if($_GET["action"] == "delete")
+      {
+           foreach($_SESSION["shopping_cart"] as $keys => $values)
+           {
+                if($values["item_id"] == $_GET["id"])
+                {
+                     unset($_SESSION["shopping_cart"][$keys]);
+                     echo '<script>alert("Item Removed")</script>';
+                     echo '<script>window.location="shop.php"</script>';
+                }
+           }
+      }
+ }
+ ?>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <title>BasePro Catalog</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body>
+    <!--Navigation Bar Code--------------------------------------------------------------------->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="projectIndex.html">Base<img src = 'logo.png' width="25" height="25">Pro</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!--Nav Items-->
+              <li class="nav-item">
+              <a class="nav-link" href="projectIndex.html">Home</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="shop.php">Shop</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Account</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="cart.php">Shopping Cart</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!--Title------------------>
+        <div align = center><h1 class="display-1" style= "font-family:'News GothicMT'">Base<img src = 'logo.png' width="100" height="100">Pro</h1></div>
+        <div class="container">
+        <form action = 'shop.php' method = 'post'>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon1" name = "search">
+            <button class="btn btn-outline-secondary" type="Search" id="button-addon1" name = 'searchButton'>Submit</button>
+          </div>
+        </form>
+    
+    <!--Shopping Page---------------------------------------------------------------->
+    <?php 
+    if(!isset($_POST['searchButton']))
+    {
+    ?>
+    <!--Db connection-->
+    
+    <div class="grid text-center" style="--bs-columns: 3;">
+    <?php
+        $query = "SELECT * FROM product ORDER BY id ASC";
+        $result = mysqli_query($connect, $query);
+        if(mysqli_num_rows($result) > 0)
+        {
+                while($row = mysqli_fetch_array($result))
+                {
+        ?>
+    <!--Displaying Items-->
+    
+                <form method="post" action="shop.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
+                        <img src="<?php echo $row["image"]; ?>" class="img-responsive"/><br />
+                        <h4><?php echo $row["name"]; ?></h4>
+                        <h4><small class="text-muted">$ <?php echo $row["price"]; ?></small></h4>
+                        <input type="text" name="quantity" class="form-control" value="1" />
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
+                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+                        <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+                    </div>
+                </form>
+        
+    <?php
+            }
+    }
+  }else{
+    ?>
+    <div class="grid text-center" style="--bs-columns: 3;">
+
+<?php
+        $target = $_POST['search'];
+        $query = "SELECT * FROM product WHERE name like '%$target%' ORDER BY id ASC";
+        $result = mysqli_query($connect, $query);
+        if(mysqli_num_rows($result) > 0)
+        {
+                while($row = mysqli_fetch_array($result))
+                {
+        ?>
+    <!--Displaying Items-->
+    
+                <form method="post" action="shop.php?action=add&id=<?php echo $row["id"]; ?>">
+                    <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
+                        <img src="<?php echo $row["image"]; ?>" class="img-responsive"/><br />
+                        <h4><?php echo $row["name"]; ?></h4>
+                        <h4><small class="text-muted">$ <?php echo $row["price"]; ?></small></h4>
+                        <input type="text" name="quantity" class="form-control" value="1" />
+                        <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
+                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
+                        <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+                    </div>
+                </form>
+        
+    <?php
+            }
+    }
+    ?>
+
+    <?php 
+  }
+    ?>
+    
+    </div>
+
+    
+    
+
+    
+
+
+                        </div>
+    <!--Footer---------------------------------------------------------------------------------------------->
+    <footer class="text-center text-white bg-primary">
+      <div class="container p-4">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5>Base Pro Cares</h5>
+    
+            <p>
+              Here at Base Pro, we are passionate about baseball and <br> hope to share our passion with the world. We are flexible
+              <br>with returns to ensure our customers get the best service!
+            </p>
+          </div>
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5>Contact Us!</h5>
+              862-435-1344<br>
+              901-274-1109<br>
+              BasePros@baseball.com<br>
+              customersupport@baseball.com
+          </div>
+        </div>
+      </div>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        @ 2022 Copyright: Melvin Matias & Justin Pernell
+      </div>
+    </footer>
+   
+  </body>
+</html>
